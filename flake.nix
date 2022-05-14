49c3db5454297c0f3aa821ab03b1fa66864f4ef9
Norbert Melzer <timmelzer@gmail.com>
2022-05-14 12:57:34 +0200

    flake: some compatibility changes

--- flake.nix
+++ flake.nix
@@ -6,12 +6,14 @@
   outputs = {self, nixpkgs, zsa, ... }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
-    devShell.x86_64-linux = pkgs.mkShell {
+    devShell.x86_64-linux = self.x86_64-linux.default; # For compatibility with nix 2.7 and older
+    devShells.x86_64-linux.default = pkgs.mkShell {
       packages = [ pkgs.wally-cli pkgs.qmk pkgs.gcc-arm-embedded pkgs.pkgsCross.avr.buildPackages.gcc8 pkgs.avrdude pkgs.dfu-programmer pkgs.dfu-util ];
     };
 
     packages.x86_64-linux.firmware = pkgs.callPackage ./firmware.nix { inherit zsa; };
 
+    apps.x86_64-linux.default = self.apps.x86_64-linux.push;
     apps.x86_64-linux.push.type = "app";
     apps.x86_64-linux.push.program = "${pkgs.writeShellScript "push" ''
       ${pkgs.wally-cli}/bin/wally-cli ${self.packages.x86_64-linux.firmware}
